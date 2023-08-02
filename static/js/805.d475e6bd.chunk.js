bef56ad54db6834b1940c5313e67e61ca63d8d5f
X777X777X <119849170+X777X777X@users.noreply.github.com>
2023-08-02 16:25:34 +0000

    Deploying to gh-pages from @ X777X777X/goit-react-hw-05-movies-02@b1677d295999f96648cb0a247eab9194a8e672b3 🚀

---
+++ static/js/805.d475e6bd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,r,t){t.r(r);t(791);var c=t(87),a=t(990),o=t(184);r.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("img",{src:a,alt:"Error 404"}),(0,o.jsx)(c.rU,{to:"/",children:"Back to home"})]})}},990:function(e,r,t){e.exports=t.p+"static/media/Error404.d5f34e7dd639acfc5456.jpeg"}}]);
+//# sourceMappingURL=805.d475e6bd.chunk.js.map
